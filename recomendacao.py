df05d04a7cbaa98cdf274dc81485a6b6ce628622
Cezar Tosta <cezar.tosta@hotmail.com>
2024-03-30 19:19:24 -0300

    Função euclidiana()
    Ajuste do nome dos dados.
    Implementação da função euclidiana() que calcula a distância Euclidiana entre dois usuários, com base nos filmes em comum.

--- recomendacao.py
+++ recomendacao.py
@@ -1,4 +1,4 @@
-avaliacoesUsuario = {'Ana': 
+avaliacoes = {'Ana': 
 		{'Freddy x Jason': 2.5, 
 		 'O Ultimato Bourne': 3.5,
 		 'Star Trek': 3.0, 
@@ -48,4 +48,15 @@ avaliacoesUsuario = {'Ana':
 	     'Exterminador do Futuro':4.0}
 }
 
+from math import sqrt
 
+def euclidiana(usuario1, usuario2):
+    si = {}
+    for item in avaliacoes[usuario1]:
+        if item in avaliacoes[usuario2]:
+            si[item] = 1
+    if len(si) == 0:
+        return 0
+    soma = sum([pow(avaliacoes[usuario1][item] - avaliacoes[usuario2][item], 2)
+                for item in avaliacoes[usuario1] if item in avaliacoes[usuario2]])
+    return 1/(1+sqrt(soma))
